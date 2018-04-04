ce11b1182e7d28040387f697ce760765880a2fc7
Ryan <ryan@buckettechnologies.com>
2018-04-04 10:07:18 -0400

    Updating the bucketSDK code that stands alone

--- bucketsdk/src/main/java/com/bucket/bucketsdk/Bucket.kt
+++ bucketsdk/src/main/java/com/bucket/bucketsdk/Bucket.kt
@@ -20,12 +20,21 @@ import java.util.*
 
 class Bucket {
     companion object {
-        var requestQueue : RequestQueue = Volley.newRequestQueue(Bucket.appContext)
+        @JvmStatic var requestQueue : RequestQueue? = null
         @SuppressLint("SimpleDateFormat") @JvmStatic val df : DateFormat = SimpleDateFormat("yyyyMMdd")
+
         @JvmStatic var appContext : Context? = null
+            set(value) {
+                // Make this a regular setter:
+                field = value
+                if (!value.isNil) {
+                    this.requestQueue = Volley.newRequestQueue(value!!)
+                }
+            }
+
         @JvmStatic var environment : Bucket.DeploymentEnvironment = Bucket.DeploymentEnvironment.Development
-        @JvmStatic var denoms : List<Int> = listOf(10000, 5000, 2000, 1000, 500, 200)
-        @JvmStatic fun bucketAmount(changeDueBack: Int): Int {
+        @JvmStatic private var denoms : List<Int> = listOf(10000, 5000, 2000, 1000, 500, 200)
+        @JvmStatic fun bucketAmount(changeDueBack: Long): Long {
             var bucketAmount = changeDueBack
             // Make sure this is ordered by the amount
             val filteredDenoms = denoms.filter { it <= changeDueBack }
@@ -37,8 +46,9 @@ class Bucket {
 
             return bucketAmount
         }
+
     }
-    abstract class Callbacks {
+    class Callbacks {
         abstract class RetailerLogin {
             abstract fun didLogIn()
             abstract fun didError(error: VolleyError?)
@@ -51,42 +61,51 @@ class Bucket {
 
     class Retailer {
         companion object {
-            @JvmStatic fun logInWith(password: String, username: String, callback: Bucket.Callbacks.RetailerLogin?): JsonObjectRequest {
+            @JvmStatic fun logInWith(password: String, username: String, callback: Bucket.Callbacks.RetailerLogin?) {
 
                 val json = JSONObject()
-
                 json.put("password", password)
                 json.put("username", username)
 
                 val url = Bucket.environment.retailerLogin().build().toString()
 
-                return JsonObjectRequest(Request.Method.POST, url, json, Response.Listener { _ ->
-                    callback.let { callback!!.didLogIn() }
+                val request = JsonObjectRequest(Request.Method.POST, url, json, Response.Listener { responseJSON ->
+
+                    val clientId = responseJSON.getString("client_id")
+                    val clientSecret = responseJSON.getString("client_secret")
+
+                    // TODO:  We need to go & test to make sure this works once we have the retailer login endpoint.
+                    if (clientId.isNullOrEmpty() || clientSecret.isNullOrEmpty()) return@Listener
+
+                    // Now go & set the values securely:
+                    Bucket.Credentials.setClientId(clientId)
+                    Bucket.Credentials.setClientSecret(clientSecret)
+
+                    // Notify our listeners:
+                    callback?.didLogIn()
+
                 }, Response.ErrorListener { error ->
-                    callback.let { callback!!.didError(error) }
+                    callback?.didError(error)
                 })
+
+                Bucket.requestQueue?.add(request)
+
             }
         }
     }
 
     class Transaction(var amount: Int, var clientTransactionId: String) {
 
-        var customerCode                 : String?  = null
-        var qrCodeContent       : URL = URL("https://test.com")
+        // This is the primary key for the transaction in our db, as annotated:
+        @PrimaryKey var bucketTransactionId : String? = null
+        var customerCode                    : String?  = null
+        var qrCodeContent                   : URL? = null
 
         // The rest of these fields are specified by the retailer:
-        var intervalId          : String? = null
-        var locationId          : String? = null
-        var terminalId          : String? = null
+        var intervalId                      : String? = null
 
-        // Primary key (for bucket)
-        var bucketTransactionId : String? = null
-
-        init {
-            this.intervalId = ""
-            this.locationId = "Ryan's Temporary Location"
-            this.terminalId = Build.SERIAL
-        }
+        var locationId                      : String? = null
+        var terminalId                      : String = Build.SERIAL
 
         private fun updateWith(json: JSONObject) {
             this.clientTransactionId = json.getString("clientTransactionId")
@@ -95,7 +114,9 @@ class Bucket {
 
             // Now take care of the qrCode contents:
             val qrCodeContent = json.getString("qrCodeContent")
-            qrCodeContent.let { this.qrCodeContent = URL(qrCodeContent) }
+            if (!qrCodeContent.isNil) {
+                this.qrCodeContent = URL(qrCodeContent)
+            }
 
         }
 
@@ -103,14 +124,17 @@ class Bucket {
 
             val obj = JSONObject()
 
-            // We will always set the amount when sending the JSON:
-            obj.put("amount", amount)
+            // Set the intervalId to this date:
             intervalId = Date().toYYYYMMDD
-            locationId.let { obj.put("locationId", it) }
-            clientTransactionId.let { obj.put("clientTransactionId", it) }
-            terminalId.let { obj.put("terminalId", it) }
-            customerCode.let { obj.put("customerCode", it) }
-            qrCodeContent.let { obj.put("qrCodeContent", it) }
+            // We will always set the amount & clientTransactionId when sending the JSON:
+            obj.put("amount", amount)
+            obj.put("clientTransactionId", clientTransactionId)
+            obj.put("intervalId", intervalId)
+            obj.put("terminalId", terminalId)
+
+            locationId.let { obj.put("locationId", it!!) }
+            customerCode.let { obj.put("customerCode", it!!) }
+            qrCodeContent.let { obj.put("qrCodeContent", it!!) }
 
             return obj
         }
@@ -121,10 +145,13 @@ class Bucket {
             val clientId = Bucket.Credentials.clientId()
             val clientSecret = Bucket.Credentials.clientSecret()
 
-            if (clientId == null || clientSecret == null) {
-
-                callback.let { it!!.didError(VolleyError("You need a client id & a client secret to create a transaction.")) }
+            var shouldIReturn = false
+            if (clientId.isNullOrEmpty() || clientSecret.isNullOrEmpty()) {
+                shouldIReturn = true
+                callback?.didError(VolleyError("You need a client id & a client secret to create a transaction."))
             }
+
+            if (shouldIReturn) return
 
             val urlBuilder = Bucket.environment.transaction()
 
@@ -139,16 +166,19 @@ class Bucket {
 
 
             // Okay we need to go & create the request & send the information to Marco:
-            val request = JsonObjectRequest(Request.Method.POST, url, httpBody, Response.Listener<JSONObject> { response ->
+            val request = JsonObjectRequest(Request.Method.POST, url, httpBody, Response.Listener { response ->
                 // Deal with the response object:
                 this.updateWith(response)
-                callback.let { it!!.transactionCreated() }
+                callback?.transactionCreated()
 
             }, Response.ErrorListener { error ->
-                callback.let { it!!.didError(error) }
+
+                // Tell the listener that we had an error:
+                callback?.didError(error)
+
             })
 
-            Bucket.requestQueue.add(request)
+            Bucket.requestQueue?.add(request)
 
         }
 
@@ -156,7 +186,7 @@ class Bucket {
 
     object Credentials {
 
-        fun clientId(): String? {
+        @JvmStatic fun clientId(): String? {
             // The only thing hardcoded here is the /clientId.  This makes it so the hacker would need the actual device to read the clientId or clientSecret, which would mean we already have a security breach.
             @SuppressLint("HardwareIds") var theKey = Settings.Secure.getString(Bucket.appContext?.contentResolver, Settings.Secure.ANDROID_ID)
             theKey += "/clientId"
@@ -164,21 +194,21 @@ class Bucket {
 
         }
 
-        fun setClientId(value: String) {
+        @JvmStatic fun setClientId(value: String) {
             // The only thing hardcoded here is the /clientId.  This makes it so the hacker would need the actual device to read the clientId or clientSecret, which would mean we already have a security breach.
             @SuppressLint("HardwareIds") var theKey = Settings.Secure.getString(appContext?.contentResolver, Settings.Secure.ANDROID_ID)
             theKey += "/clientId"
             SecurePreferences.setValue(theKey, value)
         }
 
-        fun clientSecret(): String? {
+        @JvmStatic fun clientSecret(): String? {
             // The only thing hardcoded here is the /clientSecret.  This makes it so the hacker would need the actual device to read the clientId or clientSecret, which would mean we already have a security breach.
             @SuppressLint("HardwareIds") var theKey = Settings.Secure.getString(appContext?.contentResolver, Settings.Secure.ANDROID_ID)
             theKey += "/clientSecret"
             return SecurePreferences.getStringValue(theKey, null)
         }
 
-        fun setClientSecret(value: String) {
+        @JvmStatic fun setClientSecret(value: String) {
             // The only thing hardcoded here is the /clientSecret.  This makes it so the hacker would need the actual device to read the clientId or clientSecret, which would mean we already have a security breach.
             @SuppressLint("HardwareIds") var theKey = Settings.Secure.getString(appContext?.contentResolver, Settings.Secure.ANDROID_ID)
             theKey += "/clientSecret"
@@ -217,10 +247,6 @@ class Bucket {
         }
 
         // PRE-BUILT ENDPOINT PATHS:
-        fun redeem(): Uri.Builder {
-            return this.bucketBaseUri().appendPath("redeem")
-        }
-
         fun transaction(): Uri.Builder {
             return this.bucketBaseUri().appendPath("transaction")
         }
@@ -230,6 +256,11 @@ class Bucket {
         }
     }
 }
+
+annotation class PrimaryKey
+var Any?.isNil : Boolean
+    get() { return this == null }
+    private set(value) {}
 
 var Date.toYYYYMMDD : String
     get() { return Bucket.df.format(this) }
