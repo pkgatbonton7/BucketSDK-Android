0e0fdfead46558db257826b5d5e98829fc4571ce
Ryan <ryan@buckettechnologies.com>
2018-04-11 12:03:02 -0400

    Changing the test to check for the package name since this was changed

--- bucketsdk/src/androidTest/java/bucket/sdk/ExampleInstrumentedTest.java
+++ bucketsdk/src/androidTest/java/bucket/sdk/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("bucket.bucketsdk", appContext.getPackageName());
+        assertEquals("bucket.sdk", appContext.getPackageName());
     }
 }
